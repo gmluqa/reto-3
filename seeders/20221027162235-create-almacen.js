60534eebfc60b3341e5a04532768df5acae4137b
Liza <Lizacastdiaz@gmail.com>
2022-10-27 18:45:09 +0200

    seeders

--- seeders/20221027162235-create-almacen.js
+++ seeders/20221027162235-create-almacen.js
@@ -2,19 +2,45 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('almacen', [
+      { 
+        codigo: 1,
+        lugar: "Madrid",
+        capacidad: 5090
+
+      },
+      { 
+        codigo: 2,
+        lugar: "valencia",
+        capacidad: 500
+
+      },
+      { 
+        codigo: 3,
+        lugar: "Mavalri",
+        capacidad: 600
+
+      },
+      { 
+        codigo: 4,
+        lugar: "barcelona",
+        capacidad: 8000
+
+      },
+      { 
+        codigo: 5,
+        lugar: "Marsella",
+        capacidad: 5700
+
+      },
+
+    ], {});
+
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
--- seeders/20221027162241-create-caja.js
+++ seeders/20221027162241-create-caja.js
@@ -2,19 +2,51 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('caja', [
+      { 
+        numReferencia: 1,
+        contenido: "papas",
+        valor: 707,
+        almacen: 1,
+        onCreate:" 2022-10-27"
+      },
+      { 
+        numReferencia: 2,
+        contenido: "eoeoes",
+        valor: 400,
+        almacen: 2,
+        onCreate:" 2022-10-27"
+      },
+      { 
+        numReferencia: 3,
+        contenido: "zanahorias",
+        valor: 500,
+        almacen: 3,
+        onCreate:" 2022-10-27"
+      },
+      { 
+        numReferencia: 4,
+        contenido: "zapatos",
+        valor: 599,
+        almacen: 4,
+        onCreate:" 2022-10-27"
+      },
+      { 
+        numReferencia: 5,
+        contenido: "tomates",
+        valor: 507,
+        almacen: 5,
+        onCreate:" 2022-10-27"
+      },
+      
+      
+    ], {});
+
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
